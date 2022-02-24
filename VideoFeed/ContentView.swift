3d981aff91a264e353794c76518d67077a79e07a
herveGuigoz <herve.guigoz@gmail.com>
2022-02-24 12:44:56 +0100

    Initial Commit

---
+++ VideoFeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoFeed
+//
+//  Created by Herve Guigoz on 24/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VideoFeed/VideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoFeedApp.swift
+//  VideoFeed
+//
+//  Created by Herve Guigoz on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
